ff5a75dd2b861513551ba0e04c34994a01d6f4e6
Benhin <benhinmwendwa7@gmail.com>
2024-05-10 02:20:24 +0300

    introduced a fetch method and fetched the API

--- src/Pages/Discover.js
+++ src/Pages/Discover.js
@@ -10,4 +10,14 @@ const Discover = () => {
 
     useEffect(() => { // introduced use effect hook
         const fetchRecommendedTracks = async () => {
-         
+            const recommendedTracksUrl = 'https://spotify23.p.rapidapi.com/recommendations/?seed_artists=0oSGxfWSnnOXhD2fKuz2Gy&seed_tracks=3dRfiJ2650SZu6GbydcHNb&seed_genres=&limit=100';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': '3509af7823msha08596116c3770ap1b2ab7jsna490778b83c0',
+                    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+                }
+            };
+          
+
+export default Discover;
